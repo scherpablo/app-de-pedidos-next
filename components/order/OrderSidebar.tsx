4f7df745aaf4987a8a7adfd4b3f7964f24ecde15
scherpablo <scherpablo@gmail.com>
2024-04-03 15:49:48 -0300

    obtaining categories from the DB

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,8 +1,14 @@
-const OrderSidebar = () => {
-  return (
-    <aside className="md:w-72 md:h-screen bg-white">
-        OrderSidebar
-    </aside>
-  )
+import { prisma } from "@/src/lib/prisma";
+
+async function getCategories() {
+  return await prisma.category.findMany();
 }
-export default OrderSidebar
+
+const OrderSidebar = async () => {
+  const categories = await getCategories();
+
+  console.log(categories);
+
+  return <aside className="md:w-72 md:h-screen bg-white">OrderSidebar</aside>;
+};
+export default OrderSidebar;
