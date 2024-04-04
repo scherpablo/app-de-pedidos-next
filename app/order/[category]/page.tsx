b065c53a8b3c33384a689afbac924c1ba20c01e6
scherpablo <scherpablo@gmail.com>
2024-04-04 18:58:27 -0300

    add h1 to products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,4 @@
 import ProductCard from "@/components/products/ProductCard";
-import { categories } from "@/prisma/data/categories";
 import { prisma } from "@/src/lib/prisma";
 
 const getProducts = async (category: string) => {
@@ -13,13 +12,31 @@ const getProducts = async (category: string) => {
   return products;
 };
 
+const getCategoryName = async (categorySlug: string) => {
+  const category = await prisma.category.findMany({
+    where: {
+      slug: categorySlug,
+    },
+    select: {
+      name: true,
+    },
+  });
+  const categoryName = category[0]?.name || '';
+  return categoryName;
+};
+
 const OrderPage = async ({ params }: { params: { category: string } }) => {
+  const categoryName = await getCategoryName(params.category);
   const products = await getProducts(params.category);
 
   return (
     <>
-    <h1 className="text-4xl text-center font-black">Mostrando productos para {params.category}</h1>
-      <p className="text-2xl font-semibold mt-5 mb-10 text-center">Elije y personaliza tu pedido a continuación</p>
+      <h1 className="text-4xl text-center font-black">
+        Mostrando los productos para {categoryName}
+      </h1>
+      <p className="text-2xl font-semibold mt-5 mb-10 text-center">
+        Elije y personaliza tu pedido a continuación
+      </p>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
