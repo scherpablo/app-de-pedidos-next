b73e10ebde10e04cc968946d31c8ea575e760e52
scherpablo <scherpablo@gmail.com>
2024-04-03 22:51:07 -0300

    detecting current category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,20 @@
-const OrderPage = ({params} : {params : {category : string}}) => {
-  console.log(params.category)
-  return <div>OrderPage</div> ;
+import { prisma } from "@/src/lib/prisma";
+
+const getProducts = async (category: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+  return products;
+};
+
+const OrderPage = async ({ params }: { params: { category: string } }) => {
+  const products = await getProducts(params.category);
+  console.log(products);
+
+  return <div>OrderPage</div>;
 };
 export default OrderPage;
