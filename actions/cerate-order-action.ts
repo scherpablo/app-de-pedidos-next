ee6b39b18edfb04d591704f38230d57506512b89
scherpablo <scherpablo@gmail.com>
2024-04-06 12:13:25 -0300

    enter data into the DB

--- actions/cerate-order-action.ts
+++ actions/cerate-order-action.ts
@@ -1,21 +1,34 @@
-"use server"
+"use server";
 
-import { orderSchema } from "@/src/schema"
+import { prisma } from "@/src/lib/prisma";
+import { orderSchema } from "@/src/schema";
 
-const createOrder = async (data: unknown)=>{
-    const result = orderSchema.safeParse(data)
+const createOrder = async (data: unknown) => {
+  const result = orderSchema.safeParse(data);
 
-    if(!result.success) {
-        return {
-            errors: result.error.issues
-        }
-    }
+  if (!result.success) {
+    return {
+      errors: result.error.issues,
+    };
+  }
 
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await prisma.order.create({
+      data: {
+        name: result.data.name,
+        phone: result.data.phone,
+        total: result.data.totalOrder,
+        orderProducts: {
+          create: result.data.order.map((product) => ({
+            productId: product.id,
+            quantity: product.quantity,
+          })),
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export {createOrder}
+export { createOrder };
