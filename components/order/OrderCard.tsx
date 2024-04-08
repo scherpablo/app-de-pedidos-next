556c5b3ccdf6450513b1bf37dea84b2ede56bb93
scherpablo <scherpablo@gmail.com>
2024-04-07 22:03:32 -0300

    showing orders in admin panel

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -10,21 +10,31 @@ const OrderCard = ({ order }: OrderCardProps) => {
     <>
       <section
         aria-labelledby="summary-heading"
-        className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
+        className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4 shadow-lg"
       >
-        <p className="text-2xl font-medium text-gray-900">
-          Cliente: <span className="font-bold">{order.name}</span>
+        <p className="text-2xl font-bold">
+          Cliente: <span className="font-semibold">{order.name}</span>
         </p>
-        <p className="text-lg font-medium text-gray-900">
-          Productos Ordenados:
-        </p>
+        <p className="text-lg font-semibold">Detalle del Pedido:</p>
         <dl className="mt-6 space-y-4">
+          {order.orderProducts.map((product) => (
+            <div
+              key={product.productId}
+              className="flex items-center gap-2 border-gary-200 pt-4"
+            >
+              <dt className="flex items-center">
+                <span className="font-bold">
+                  ({product.quantity}) {""}
+                </span>
+              </dt>
+              <dd className="italic font-semibold">{product.product.name}</dd>
+            </div>
+          ))}
           <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-            <dt className="text-base font-medium text-gray-900">
-              Total a Pagar:{" "}
-              <span className="font-bold">{formatCurrency(order.total)}</span>
-            </dt>
-            <dd className="text-base font-medium text-gray-900">{}</dd>
+            <dt className="font-semibold text-lg">Total a Pagar: </dt>
+            <dd className="text-xl font-bold">
+              {formatCurrency(order.total)}
+            </dd>
           </div>
         </dl>
 
