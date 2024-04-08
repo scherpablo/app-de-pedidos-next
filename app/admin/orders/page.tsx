f7b941f988a6bb0ae6dc2eaf9a7cf9ef8b227c99
scherpablo <scherpablo@gmail.com>
2024-04-07 21:43:58 -0300

    showing pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import Title from "@/components/ui/Title";
 import { prisma } from "@/src/lib/prisma";
@@ -6,26 +7,39 @@ import { orderSchema } from "@/src/schema";
 const getPendingOrders = async () => {
   const orders = await prisma.order.findMany({
     where: {
-      status: false
+      status: false,
     },
     include: {
       orderProducts: {
         include: {
-          product: true
-        }
-      }
-    }
-  })
-  return orders
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
 };
 
 const OrderPage = async () => {
-const orders = await getPendingOrders();
+  const orders = await getPendingOrders();
 
   return (
     <>
       <Title>Panel de Administración Pedidos</Title>
       <Heading>Administrar Pedidos</Heading>
+
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-5 mt-5">
+          {orders.map((order) => (
+            <OrderCard 
+            key={order.id}
+            order={order}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">No hay pedidos pendientes</p>
+      )}
     </>
   );
 };
