b9059c4efa65588a2ed69c07f143395d24b4f80b
scherpablo <scherpablo@gmail.com>
2024-04-07 21:24:09 -0300

    obtaining pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,7 +1,27 @@
 import Heading from "@/components/ui/Heading";
 import Title from "@/components/ui/Title";
+import { prisma } from "@/src/lib/prisma";
+import { orderSchema } from "@/src/schema";
 
-const OrderPage = () => {
+const getPendingOrders = async () => {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+  return orders
+};
+
+const OrderPage = async () => {
+const orders = await getPendingOrders();
+
   return (
     <>
       <Title>Panel de Administración Pedidos</Title>
