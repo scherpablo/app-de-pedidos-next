8c30309b85f730d1bc8b21e5656795acc7ae5abc
scherpablo <scherpablo@gmail.com>
2024-04-09 07:49:31 -0300

    showing products on a table

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,22 @@
+import ProductsTable from "@/components/admin/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import Title from "@/components/ui/Title";
+import { prisma } from "@/src/lib/prisma";
 
-const ProductsPage = () => {
+const getProducts = async () => {
+  const products = await prisma.product.findMany();
+
+  return products;
+};
+
+const ProductsPage = async () => {
+  const products = await getProducts();
+
   return (
     <>
       <Title>Panel de Administración Productos</Title>
       <Heading>Adminstrar Productos</Heading>
+      <ProductsTable products={products} />
     </>
   );
 };
