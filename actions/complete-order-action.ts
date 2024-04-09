f76bfd50bb150c8c3ffdbddc63531df462eb46ca
scherpablo <scherpablo@gmail.com>
2024-04-08 23:05:13 -0300

    mark orders as completed

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,7 +1,23 @@
 "use server"
 
-const completeOrder = async () => {
-    console.log('desde complete order')
+import { prisma } from "@/src/lib/prisma";
+
+const completeOrder = async (formData: FormData) => {
+    const orderId = formData.get('order-id')! //el simbolo ! indica que el valor es confiable
+
+    try {
+        await prisma.order.update({
+            where: {
+                id: +orderId //el + convierte a orderId de tipo string a number
+            },
+            data: {
+                status: true,
+                orderReadyAt: new Date(Date.now())
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default completeOrder;
--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -33,13 +33,12 @@ const OrderCard = ({ order }: OrderCardProps) => {
           ))}
           <div className="flex items-center justify-between border-t border-gray-200 pt-4">
             <dt className="font-semibold text-lg">Total a Pagar: </dt>
-            <dd className="text-xl font-bold">
-              {formatCurrency(order.total)}
-            </dd>
+            <dd className="text-xl font-bold">{formatCurrency(order.total)}</dd>
           </div>
         </dl>
 
         <form action={completeOrder}>
+          <input type="hidden" value={order.id} name="order-id" />
           <input
             type="submit"
             className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer rounded-lg"
