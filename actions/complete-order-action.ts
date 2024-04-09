bcd3c0d08c44a679d90eb74f3ffdc956625730cb
scherpablo <scherpablo@gmail.com>
2024-04-08 23:10:51 -0300

    revalidate path

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,23 +1,26 @@
-"use server"
+"use server";
 
+import { revalidatePath } from "next/cache";
 import { prisma } from "@/src/lib/prisma";
 
 const completeOrder = async (formData: FormData) => {
-    const orderId = formData.get('order-id')! //el simbolo ! indica que el valor es confiable
+  const orderId = formData.get("order-id")!; //el simbolo ! indica que el valor de orderId existe
 
-    try {
-        await prisma.order.update({
-            where: {
-                id: +orderId //el + convierte a orderId de tipo string a number
-            },
-            data: {
-                status: true,
-                orderReadyAt: new Date(Date.now())
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await prisma.order.update({
+      where: {
+        id: +orderId, //el + convierte a orderId de tipo string a number
+      },
+      data: {
+        status: true,
+        orderReadyAt: new Date(Date.now()),
+      },
+    });
+
+    revalidatePath(`/admin/orders`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default completeOrder;
