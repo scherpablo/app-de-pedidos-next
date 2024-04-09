2dfb98fd2cd59d47b2cc1ada6f06c1a6522414e9
scherpablo <scherpablo@gmail.com>
2024-04-09 19:08:07 -0300

    displaying search results

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,11 +1,46 @@
+import FormProductSearch from "@/components/admin/FormProductSearch";
+import ProductsTable from "@/components/admin/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-const SearchPage = async ({ searchParams } : { searchParams: { search: string } } ) => {
-console.log(searchParams.search)
+const serachProducts = async (searchTerm: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: "insensitive",
+      },
+    },
+    include: {
+      category: true,
+    },
+  });
+  return products;
+};
+
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) => {
+  const products = await serachProducts(searchParams.search);
 
   return (
     <>
-      <Heading>Resultados de Búsqueda</Heading>
+      <Heading>
+        Resultados de búsqueda para:{" "}
+        <span className="font-bold uppercase">{searchParams.search}</span>
+      </Heading>
+      <div className="flex justify-end">
+        <FormProductSearch />
+      </div>
+      {products.length ? (
+        <ProductsTable products={products} />
+      ) : (
+        <p className="flex justify-center">
+          No se encontraron productos realcionados con la búsqueda
+        </p>
+      )}
     </>
   );
 };
