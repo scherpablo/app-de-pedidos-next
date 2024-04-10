27cdee3a1f2bdaccccfa2b0ab0474ae26e59159b
scherpablo <scherpablo@gmail.com>
2024-04-09 23:08:20 -0300

    creating image upload component

--- components/admin/ImageUpload.tsx
+++ components/admin/ImageUpload.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { CldUploadWidget } from "next-cloudinary";
+import { TbPhotoPlus } from "react-icons/tb";
+
 const ImageUpload = () => {
   return (
     <>
-      <h1>image</h1>
+      <CldUploadWidget
+        uploadPreset="ndseojzg"
+        options={{ maxFiles: 1 }}
+        onSuccess={(result, { widget }) => {
+            console.log(result)
+        }}
+      >
+        {({ open }) => (
+          <>
+            <div className="space-y-2">
+              <label className="text-slate-800">
+                <div
+                  onClick={() => open()}
+                  className="realtive cursor-pointer hover:opacity-70 transition p-10 
+                border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
+                >
+                  <TbPhotoPlus size={50} />
+                  <p className="text-lg font-semibold">Agergar Imagen</p>
+                </div>
+              </label>
+            </div>
+          </>
+        )}
+      </CldUploadWidget>
     </>
   );
 };
