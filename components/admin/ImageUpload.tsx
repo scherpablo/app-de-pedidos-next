2fc3f0446f7693a324fab905f497dc0a4b4ef90e
scherpablo <scherpablo@gmail.com>
2024-04-09 23:40:57 -0300

    get image url from cloudinary

--- components/admin/ImageUpload.tsx
+++ components/admin/ImageUpload.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import Image from "next/image";
+import { useState } from "react";
 import { CldUploadWidget } from "next-cloudinary";
 import { TbPhotoPlus } from "react-icons/tb";
 
 const ImageUpload = () => {
+  const [imageUrl, setImageUrl] = useState("");
+
   return (
     <>
       <CldUploadWidget
         uploadPreset="ndseojzg"
         options={{ maxFiles: 1 }}
         onSuccess={(result, { widget }) => {
-            console.log(result)
+          if (result.event === "success") {
+            widget.close();
+            //@ts-ignore
+            setImageUrl(result.info?.secure_url);
+          }
         }}
       >
         {({ open }) => (
@@ -19,14 +27,27 @@ const ImageUpload = () => {
               <label className="text-slate-800">
                 <div
                   onClick={() => open()}
-                  className="realtive cursor-pointer hover:opacity-70 transition p-10 
+                  className="relative cursor-pointer hover:opacity-70 transition p-10 
                 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
                 >
                   <TbPhotoPlus size={50} />
                   <p className="text-lg font-semibold">Agergar Imagen</p>
+
+                  {imageUrl && (
+                    <div className="absolute inset-0 w-full h-full">
+                      <Image
+                        fill
+                        style={{ objectFit: "contain" }}
+                        src={imageUrl}
+                        alt="Imagen Producto"
+                      />
+                    </div>
+                  )}
                 </div>
               </label>
             </div>
+
+            <input type="hidden" name="image" value={imageUrl} />
           </>
         )}
       </CldUploadWidget>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+        ],
+        // domains: ['**.com'],
+        // dangerouslyAllowSVG: true,
+        // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        // unoptimized: true,
+    }
+};
 
 export default nextConfig;
