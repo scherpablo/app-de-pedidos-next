a143b26280f465b4cb6906409680fcb9b0626ab1
scherpablo <scherpablo@gmail.com>
2024-04-11 07:05:35 -0300

    Creating the API Endpoint and Ready Orders Screen

---
+++ app/orders/page.tsx
@@ -0,0 +1,14 @@
+import Heading from "@/components/ui/Heading";
+import Title from "@/components/ui/Title";
+
+const page = () => {
+  return (
+    <>
+      <div className="mt-5">
+        <Title>Panel de Pedidos Completados</Title>
+        <Heading>Pedidos Listos</Heading>
+      </div>
+    </>
+  );
+};
+export default page;
