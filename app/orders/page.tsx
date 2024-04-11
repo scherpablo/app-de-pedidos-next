b3318022647b16054cabb220994fde30f7c2a096
scherpablo <scherpablo@gmail.com>
2024-04-11 07:48:32 -0300

    Consume the Endpoint and show the ready orders

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,14 +1,51 @@
-import Heading from "@/components/ui/Heading";
-import Title from "@/components/ui/Title";
+"use client";
 
-const page = () => {
-  return (
-    <>
-      <div className="mt-5">
-        <Title>Panel de Pedidos Completados</Title>
-        <Heading>Pedidos Listos</Heading>
-      </div>
-    </>
-  );
+import useSWR from "swr";
+import { OrderWithProducts } from "@/src/types";
+import Heading from "@/components/ui/Heading";
+import Logo from "@/components/ui/Logo";
+import Title from "@/components/ui/Title";
+import Spinner from "@/components/ui/Spinner";
+import { DiVim } from "react-icons/di";
+import OrdrerItemList from "@/components/order/OrdrerItemList";
+
+const OrdersPage = () => {
+  const url = "/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  if (error) return <p className="text-center">Error al cargar los datos</p>;
+
+  if (data)
+    return (
+      <>
+        <div className="my-5">
+          <Title>Panel de Administración - Pedidos</Title>
+          <Heading>Pedidos Listos</Heading>
+          <div className="-mt-10">
+            <Logo />
+          </div>
+
+          {data.length ? (
+            <div className="grid grid-cols-1 gap-5 max-w-5xl mx-auto mt-10">
+              {data.map((order) => (
+                <OrdrerItemList key={order.id} order={order}/>
+              ))}
+            </div>
+          ) : (
+            <p>No hay pedidos listos</p>
+          )}
+        </div>
+      </>
+    );
 };
-export default page;
+
+export default OrdersPage;
---
+++ components/order/OrdrerItemList.tsx
@@ -0,0 +1,34 @@
+import { OrderWithProducts } from "@/src/types";
+
+type OrderItemListProps = {
+  order: OrderWithProducts;
+};
+
+const OrdrerItemList = ({ order }: OrderItemListProps) => {
+  return (
+    <div className="bg-white shadow-lg p-5 rounded-lg flex flex-col">
+      <div className="flex justify-between py-6">
+        <p className="font-bold text-lg">
+          Cliente: <span className="text-indigo-600">{order.name}</span>
+        </p>
+        <p className="font-bold text-lg">
+          Teléfono: <span className="text-indigo-600">{order.phone}</span>
+        </p>
+      </div>
+
+      <ul
+        role="list"
+        className="divide-y divide-gray-200 border-t border-gray-200 font-semibold"
+      >
+        {order.orderProducts.map((product) => (
+          <li key={product.id} className="flex gap-2 py-6">
+            <p>({product.quantity}) {""}</p>
+            <p className="italic">{product.product.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default OrdrerItemList;
